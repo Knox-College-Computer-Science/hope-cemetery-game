2968d168ed9cc26a0e5d270525ab7fa2feddebb6
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-07-24 18:54:26 -0400

    create mausoleum effects

--- data/GameSaver/game_level_saver.gd
+++ data/GameSaver/game_level_saver.gd
@@ -113,7 +113,7 @@ func save_level():
 	# Save resources
 	ResourceSaver.save(saved_game, "user://"+get_file_name(world_scene)+".tres")
 
-func load_level():
+func load_level(player_position: bool = false):
 	var saved_game = load("user://"+get_file_name(world_scene)+".tres")
 	if(!is_instance_valid(saved_game)):
 		return
